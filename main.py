d684cb942bd5d777ec7a597c5917b1b1a4facb27
stevenhuang226 <stevenhuang.dog@gmail.com>
2023-09-29 01:12:40 +0800

    main-py

---
+++ main.py
@@ -0,0 +1,21 @@
+import discord,os,datetime
+from lib.logger import write_log
+TOKEN = "MTE1Njk3NTc0OTgzNjg1MzI4MA.G7PUr7.gbh-vJ1ymcJfT3i5qRHte5XSNLuDJSNP55y758"
+# check log file
+while True():
+	log_file = input('input log file name[name => log_file/n => no log]:')
+	if (os.path.exists(log_file)):
+		break
+	elif (log_file == 'n' or log_file == 'N' or log_file == 'no' or log_file == 'No' or log_file == 'NO'):
+		log_file = False
+		break
+logger = write_log(log_file)
+logger.write('discord bot start')
+client = discord.Client()
+@client.event
+async def on_ready():
+	logger.write(f'logged as {client.user}')
+	logger.write(f'server list {", ".join([element.name for element in client.guilds])}')
+@client.event
+async def on_message():
+	
