0790f0643dfc3a61dfa89cdf3ff84856cfb048df
stevenhuang226 <stevenhuang.dog@gmail.com>
2023-09-29 16:55:46 +0800

    debug logger

--- lib/logger.py
+++ lib/logger.py
@@ -1,13 +1,14 @@
+import datetime,os
 class write_log:
-	async def __init__(self,log_file):
-		if (self.log_file == False):
+	def finit(self,log_file):
+		if (log_file == False):
 			self.log_file == False
 		else:
 			self.log_file = log_file
-	async def write(self,log_info):
+	def write(self,log_info):
 		if (self.log_file != False):
 			self.log_info = log_info
-			self.time = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+			self.time = datetime.datetime.now().strftime('%Y-%m-%d_%H:%M:%S')
 			os.system(f"echo '{self.time}|{self.log_info}' >> {self.log_file}")
 		else:
 			pass
