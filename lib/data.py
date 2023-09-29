d7afcd80ed9c3049dfd1f9f9acd920a1d81db2ff
stevenhuang226 <stevenhuang.dog@gmail.com>
2023-09-29 20:02:33 +0800

    upgrade data

--- lib/data.py
+++ lib/data.py
@@ -13,22 +13,34 @@ class data_storage:
 			# return "create table false, table has exists"
 		else:
 			split_text = list([f"CREATE TABLE {table_name} ("])
+			f_pass = True
 			for column, data_type in key_valuetype.items():
-				split_text.append(f"	{column} {data_type}")
-			split_text.append(")")
-			self.cursor.execute(('\n'.join(split_text)))
+				if f_pass:
+					split_text.append(f"\n    {column} {data_type}")
+					f_pass = False
+				else:
+					split_text.append(f",\n    {column} {data_type}")
+			split_text.append('\n)')
+			self.cursor.execute(''.join(split_text))
 			self.sql.commit()
 			# return f'{table_name} created'
-	async def insert_info(self,table_name,column,value): # column value => list
-		split_text = list([f"INSERT INFO {table_name} ({','.join(column)})"])
-		split_text.append(f"VALUES ({','.join(value)})")
-		self.cursor.execute('\n'.join(split_text))
+	async def insert_into(self,table_name,column,value): # column value => list
+		#位字串形式的值加上單引號
+		tran_values = []
+		for element in value:
+			if type(element) == int or type(element) == float:
+				tran_values.append(f"{element}")
+			else:
+				tran_values.append(f"'{element}'")
+		sql_cm = f"INSERT INTO {table_name} ({','.join(column)}) VALUES ({','.join(tran_values)})"
+		self.cursor.execute(sql_cm)
 		self.sql.commit()
 	async def update(self,table_name,colval,where): # colval => dict
 		split_text = list([f"UPDATE {table_name}","SET"])
 		for column, value in colval.items():
-			split_text.append(f"	'{column}' = '{value}'")
+			split_text.append(f"{column} = '{value}'")
 		split_text.append(f"WHERE {where}")
+		print('\n'.join(split_text))
 		self.cursor.execute('\n'.join(split_text))
 		self.sql.commit()
 	async def select_id(self,table_name,id,pdict=False):
